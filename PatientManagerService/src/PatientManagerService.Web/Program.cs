5035d67f182f66c91ce287fd3ed82f2035b1efe8
cliffordru <clifford.gray@gmail.com>
2023-02-22 15:12:44 -0500

    Add seq support

--- PatientManagerService/src/PatientManagerService.Web/Program.cs
+++ PatientManagerService/src/PatientManagerService.Web/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration).WriteTo.Seq("http://localhost:5341"));
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
